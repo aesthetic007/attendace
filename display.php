8f5c290d71ebc52b22bf05cfaf0ec8499b7986f8
aesthetic007 <86158891+aesthetic007@users.noreply.github.com>
2021-09-14 10:50:40 +0530

    Create display.php

---
+++ display.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$db = mysqli_connect("localhost","id17453594_attendance","E\#1V=Ie-\??db!?","id17453594_test");
+
+if(!$db)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
